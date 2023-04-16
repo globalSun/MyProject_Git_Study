544e0f965031358dc3ba7b3c19f549f8e5fad00e
zSun <3487593357@qq.com>
2023-04-16 15:15:15 +0800

    hot-fix first commit

--- src/main/java/com/zsun/gitstudy/hello.java
+++ src/main/java/com/zsun/gitstudy/hello.java
@@ -13,5 +13,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!This is my test!");
+        System.out.println("Hello Git!This is my test on hot-fix!");
     }
 }
