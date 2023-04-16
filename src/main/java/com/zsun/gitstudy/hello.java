0e959bef991f7b3b7f9615c89d00e83c5a93c1db
zSun <3487593357@qq.com>
2023-04-16 15:00:25 +0800

    first commit

---
+++ src/main/java/com/zsun/gitstudy/hello.java
@@ -0,0 +1,16 @@
+package com.zsun.gitstudy;
+
+/**
+ * ClassName:hello
+ * Package:com.zsun.gitstudy
+ * Description:
+ *
+ * @author zy
+ * @create 2023-04-1614:55
+ * @Version 1.0
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
