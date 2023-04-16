f2f48b08362f03087743dbbd1de625cc55870037
zSun <3487593357@qq.com>
2023-04-16 15:02:08 +0800

    second commit

---
+++ src/main/java/com/zsun/gitstudy/MyTest.java
@@ -0,0 +1,13 @@
+package com.zsun.gitstudy;
+
+/**
+ * ClassName:MyTest
+ * Package:com.zsun.gitstudy
+ * Description:
+ *
+ * @author zy
+ * @create 2023-04-1615:01
+ * @Version 1.0
+ */
+public class MyTest {
+}
